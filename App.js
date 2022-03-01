0b4304d7eb9edcd95d7c50926f76668dfa1098c6
Mostafa Mahmoud Salem <msalem@ejada.com>
2022-03-01 16:47:28 +0200

    feat: added open-sans font along with expo-font and expo-app-loading

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return <CategoriesScreen />;
 }
 
