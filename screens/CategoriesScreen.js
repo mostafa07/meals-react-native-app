f290cbdae05c6fce9c4cb2d57f288fecdb00a0d7
Mostafa Mahmoud Salem <msalem@ejada.com>
2022-03-02 16:53:58 +0200

    feat: added list of catgeories to categories screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,47 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Colors from "../constants/Colors";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoriesScreen = (props) => {
+  const CategoryGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({ routeName: "CategoryMeals" });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text>Catgeories Screen!</Text>
-      <Button
-        title="Go to Category Meals Screen"
-        onPress={() =>
-          props.navigation.navigate({ routeName: "CategoryMeals" })
-        }
-      />
-    </View>
+    <FlatList
+      numColumns={2}
+      data={CATEGORIES}
+      renderItem={CategoryGridItem}
+      keyExtractor={(item, index) => item.id}
+    />
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
@@ -20,6 +49,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 128,
   },
 });
 
