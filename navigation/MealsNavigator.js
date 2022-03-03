60e7be5a57981eae99393f5784a28b53f12925f4
Mostafa Mahmoud Salem <msalem@ejada.com>
2022-03-03 21:20:41 +0200

    feat: added bottom tab navigation with meals and favorites tabs

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,12 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 
 const MealsNavigator = createStackNavigator(
@@ -33,4 +36,34 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavoritesBottomTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarLabel: "Meals",
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name="ios-restaurant" size={24} color={tabInfo.color} />
+          );
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarLabel: "Favorites",
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-star" size={24} color={tabInfo.color} />;
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavoritesBottomTabNavigator);
