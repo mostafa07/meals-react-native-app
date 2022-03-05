e9afb641f6dbb45aba74867be6b9e120528e66e5
Mostafa Mahmoud Salem <msalem@ejada.com>
2022-03-05 15:16:39 +0200

    feat: added meal detail screen content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,24 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import CustomText from "../components/CustomText";
 import { MEALS } from "../data/dummy-data";
 
-const MealDetailScreen = (props) => {
+const ListItem = (props) => {
   return (
-    <View style={styles.screen}>
-      <Text>Meal Detail Screen!</Text>
+    <View style={styles.listItem}>
+      <CustomText>{props.children}</CustomText>
     </View>
+  );
+};
+
+const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <CustomText>{selectedMeal.duration}m</CustomText>
+        <CustomText>{selectedMeal.complexity.toUpperCase()}</CustomText>
+        <CustomText>{selectedMeal.affordability.toUpperCase()}</CustomText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam("mealId");
-  const meal = MEALS.find((it) => it.id === mealId);
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return {
-    headerTitle: meal.title,
-    headerRight: () => (
+    headerTitle: selectedMeal.title,
+    headerRight: (
       <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
         <Item
           title="Favorite"
@@ -33,10 +59,26 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
